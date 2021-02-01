4bb2b52b1d972faeb810b22a413538af297a6d8a
akhyaradn <akhyaradn7@gmail.com>
2021-02-01 18:29:09 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <div class="todo-container">
+      <todo-list></todo-list>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 24px;
+}
+
+.todo-container {
+  width : 50%;
+  margin: auto;
+}
+</style>
